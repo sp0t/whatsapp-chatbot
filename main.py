2cd08f386c6ef4530923ec398a1b0eb426f6a570
sp0t <chenyin1g992@gmail.com.com>
2024-05-15 18:48:11 -0700

    first commit

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ async def reply(Body: str = Form()):
 
     response = client.chat.completions.create(
         messages=[
-            {
-                "role": "user",
-                "content": Body,
-            }
-        ],
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
         model="gpt-3.5-turbo",
     )
 
